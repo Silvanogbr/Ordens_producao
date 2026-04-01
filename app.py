b3adf4eae01164c9b19672d773e5e6d2d5b2317c
Silvanogbr <gabriel_silva206@estudante.sesisenai.org.br>
2026-04-01 08:45:13 -0300

    commit

--- app.py
+++ app.py
@@ -1,52 +1,118 @@
-#BACK-END FLASK: Rotas da API rest
-
+#BACK-END FLASK: ROTAS DA API REST
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from database import init_bd, get_connection
 
-#Cria uma instancia da aplicação Flash
+#Cria uma instância da aplicação Flash
 app = Flask(__name__, static_folder='static', static_url_path='')
 
-#habilitar os CORS
+#Habilitar os CORS
 CORS(app)
 
-#ROTA N1 - Pagina incial
-@app.route('/') 
+#ROTA N1 - PÁGINA INICIAL ------------------------------------------
+@app.route('/')
 def index():
-    #ALIMENTAR ARQUIVO INDEX.HTML DA PASTA STATIC
+    #ALIMENTAR O ARQUIVO INDEX.HTML DA PASTA STATIC
     return app.send_static_file('index.html')
-
-#ROTA N2 - Status API
+#ROTA N2 - STATUS API
 @app.route('/status')
 def status():
-    """Rota de verificação da API(SAÚDE)
-    Retornar um JSON informando qual o servidor esta ativo"""
+    """ROTA DE VERIFICAÇÃO DA API(SAÚDE)
+    RETORNAR UM JSON INFORMANDO QUE O SERVIDOR ESTA 
+    ATIVO"""
     return jsonify({
         "status": "online",
-        "sistema": "Sistema de ordem de produção",
-        "versao": "1.0.0",
-        "mensagem": "olá, fábrica, API FUNCIONANDO!"
+        "sistema": "Sistema de ordem de Produção",
+        "versao":"1.0.0",
+        "mensagem":"Ola, Fabrica, API FUNCIONANDO!"
     })
-
-#ROTA N3 - Listar todas as ordens(get)
+#ROTA N3 - LISTAR TODAS AS ORDENS(GET) -----------------------------
 @app.route('/ordens', methods=['GET'])
 def listar_ordens():
     """
-    LISTAR TODAS AS ORDENS DA PRODUÇÃO CADASTRADAS.
+    LISTAR TODAS AS ORDENS DE PRODUÇÃO CADASTRADAS.
     MÉTODOS HTTP: GET
-    URL: http://localhost/ordens
-    Retorna: Lista e ordens em formato JSON
+    URL: http://localhost:5000/ordens
+    Retorna: Lista e ordens em formato JSON.
     """
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute('SELECT *FROM ordens ORDER BY id DESC')
+    cursor.execute('SELECT * FROM ordens ORDER BY id DESC')
     ordens = cursor.fetchall()
-    conn.close()    
-    
-    #Converte cada Row do SQLite em dicionario para serializar em JSON
+    conn.close()
+    #Converte cada Row do SQLite em discionario Python para serializar em JSON    
     return jsonify([dict(o) for o in ordens])
 
-#PONTO DE PARTIDA
+# ROTA POR ID - BUSCAR UMA ORDEM ESPECIFICA PELO ID(GET) -----------------
+
+@app.route('/ordens/<int:ordem_id>', methods=['GET'])
+
+def buscar_ordem(ordem_id):
+    """
+    Buscar uma única ordem de produção pelo ID.
+    
+    Parametros de URL:
+        - ordem id(int): ID da ordem a ser buscada.
+        
+    Retornar:
+        200 + JSON da ordem, se for encontrada.
+        404 + mensagem de erro, se não existir.
+    """
+    conn = get_connection()
+    cursor = conn.cursor()
+    #o '?' é substituido pelo valor de ordem_id de forma segura
+    cursor.execute('SELECT * FROM ordens WHERE id = ?', (ordem_id,))
+    ordem = cursor.fetchone()  #ele rotorna um único registro ou None
+    conn.close()
+    
+    if ordem is None:
+        return jsonify({'erro': f'Ordem {ordem_id} nao encontarada.'}), 404
+    return jsonify(dict(ordem)), 200
+
+
+# ROTA : CRIAR NOVA ORDEM DE PRODUÇÃO (POST)----------------------------
+@app.route('ordens', methods=['POST'])
+def criar_ordem():
+    """
+    Cria uma nova ordem de produção a partir dos dados JSON enviados.
+    
+    Body esperado(JSON):
+    
+        produto      (str) : Nome do produto     - Obrigatório
+        quantidade   (int) : Quantidade de peças - Obrigatório, > 0
+        status       (str) : Opcional            - Padrão : 'Pendente'
+    
+    Retorno:
+        201 : JSON da ordem criada, em caso de sucesso.
+        400 : mensagem de erro, se dados inválidos
+    """
+
+    dados = request.get_json()
+    
+    if not dados:
+        return jsonify({'erro': 'Body da requisicao ausente ou invalido'}), 400
+    
+    #verificação de campo obrigatório (produto)
+    produto = dados.get('produto', '').strip()
+    if not produto:
+        return jsonify({'erro': 'Campo "Produto" e obrigatorio e nao pode ser vazio.'}), 400
+    #verificação de campo obrigatório (quantidade)
+    quantidade = dados.get('quantidade')
+    if quantidade is None:
+        return jsonify({'erro': 'Campo "quantidade" e obrigatorio.'}), 400
+    #Verifica se a quantidade é um número inteiro e positivo
+    try:
+        quantidade = int(quantidade)
+        if quantidade <= 0:
+            raise ValueError()
+    except (ValueError, TypeError):
+        return jsonify({'erro': 'Campo "Quantidade" deve ser um numero inteiro e positivo.'}), 400
+    #Status (*pendente, em andamento, concluída) - opcional
+    status_validos = ['Pendente','Em andamento','Concluida']
+    status = dados.get('status', 'Pendente')
+    if status not in status_validos:
+        return jsonify({'erro': f'Status invalido. Use {status_validos}'}), 400
+#PONTO DE PARTIDA -------------------------------------------------------
 
 if __name__=='__main__':
     init_bd()
--- database.py
+++ database.py
@@ -1,52 +1,53 @@
-#Criação e configuração do banco de dados SQLite
+#CRIAÇÃO E CONFIGURAÇÃO DO BANCO DE DADO SQLITE
 import sqlite3
 
-#Constatnte coom o nome do arquivo de banco de dados
+#Constante com o nome do arquivo de banco de dados
 #O arquivo será criado durante a primeira execução
 bd_ordem = 'ordens.bd'
 
 def get_connection():
     '''
-    Cria e retorna uma coneção com o banco de dados SQLite
+    Cria e retorna uma conexão com o banco de dados SQLite
     
-    a propriedade row_factory permite acessar as colunas pelo nome
-    (ex: ordem['produto'] em vez de pelo indice(ex: ordem[1]))
+    A propriedade row_factory permite acessar as colunas pelo nome
+    (Ex: ordem['produto'] em vez de pelo índice(Ex:ordem[1]))
     
-    retorna:
-        sqlite3.connection: objeto de conexão com o banco de dados.
+    Retorna:
+        sqlite3.Connection: objeto de conexão com o banco de dados.
     '''
-
     conn = sqlite3.connect(bd_ordem)
     conn.row_factory = sqlite3.Row
     return conn
 
 def init_bd():
     '''
-    iniciliza o banco de dados criando a tabela 'ordens' se ela ainda nn existir. Seguro para chamar
-    multiplas vezes.
+    Inicializa o banco de dados criando a tabela 'ordens' se ela ainda não
+    existir. Seguro para chamar múltiplas vezes.
     '''
     conn = get_connection()
     
-    #cursor() permitir executar comandos SQL
+    # Cursor() permitir executar comandos SQL
     cursor = conn.cursor()
     
-    #if not exist garante que o comando nao falhe se a tabela ja existir 
+    #IF NOT EXIST garantir que o comando não falhe se a tabela já exixtir
     
     cursor.execute('''
                    CREATE TABLE IF NOT EXISTS ordens(
                        id INTEGER PRIMARY KEY AUTOINCREMENT,
                        produto      TEXT        NOT NULL,
                        quantidade   INTEGER     NOT NULL,
-                       status       TEXT        DEFAULT 'pendente',
-                       criado_em    TEXT        DEFAULT(datetime('now', 'localtime'))
+                       status       TEXT        DEFAULT 'Pendente',
+                       criado_em    TEXT        DEFAULT(datetime('now','localtime'))
                        )
                        ''')
     
-    #commit() salvar as alterações no arquivo .bd
+    #commit() Salvar as alterações no arquivo .bd
     conn.commit()
     
-    #close() liberar a conexão
+    #close() liberar a conexão(boa prática)
     conn.close()
     
-    print("Banco de Dados Inicializado com Sucesso")
+    print("Banco de dados inicializado com sucesso!")
 
+
+#init_bd()
